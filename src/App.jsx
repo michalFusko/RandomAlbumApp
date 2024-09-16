37769fdac589c2edf445c3647b349706957fc88f
micikodes <max.micuch@googlemail.com>
2024-09-16 19:16:52 +0200

    higogo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Login from "./components/Login"
 import AlbumCard from './components/AlbumCard'
 import Nav from "./components/Nav"
+import SeeMore from './components/seeMore'
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
     <Nav></Nav>
     <Login></Login>
     <AlbumCard></AlbumCard>
+    <SeeMore></SeeMore>
     </>
   )
 }
---
+++ src/components/seeMore.jsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useGetUserAlbumsQuery } from '../api/api';
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+export default function SeeMore() {
+  const { data : Albums, error, isLoading } = useGetUserAlbumsQuery()
+
+    
+  return (
+    <Card sx={{ minWidth: 275, textAlign: "center" }}>
+      <CardContent>
+        <Typography gutterBottom sx={{ color: 'text.primary', fontSize: 25 }}>
+          Album details
+        </Typography>
+        <Typography sx={{}} variant="h6" component="div">
+          Year of Release: HOUNO <br></br>
+          Label: HOUNO <br></br>
+          Duration: HOUNO  <br></br>
+          Number of tracks: HOUNO
+        </Typography>
+      </CardContent>
+      <CardActions>
+      </CardActions>
+    </Card>
+  );
+}
