94edb36db23d69aa88fa9a3e5907c9d365c9751a
Michal Fusko <michal.fusko28@gmail.com>
2024-09-18 13:21:54 +0200

    seemore date to time logic

--- src/components/seeMore.jsx
+++ src/components/seeMore.jsx
@@ -16,6 +16,21 @@ const bull = (
 );
 
 export default function SeeMore({randomAlbum}) {
+  console.log(randomAlbum.tracks.items[0].duration_ms)
+  const albumLength = randomAlbum.tracks.items.length
+  let albumDuriationMs = 0
+  for(let i = 0; i < albumLength; i ++){
+    albumDuriationMs += randomAlbum.tracks.items[i].duration_ms
+  }
+  console.log(albumDuriationMs)
+  console.log(albumLength)
+  const albumDuration = () => {
+    const albumTime = new Date(albumDuriationMs - 3600000)
+    console.log(albumTime.toTimeString().split(" "))
+    return albumTime.toTimeString().split(" ")[0] 
+    
+  }
+  console.log(albumDuration())
 
   return (
   
@@ -27,8 +42,8 @@ export default function SeeMore({randomAlbum}) {
         <Typography sx={{}} variant="h6" component="div">
           Release Date: {randomAlbum.release_date} <br></br>
           Label: {randomAlbum.label} <br></br>
-          Duration: HOUNO <br></br>
-          Number of tracks: {randomAlbum.tracks.items.length}
+          Duration: {albumDuration()} <br></br>
+          Number of tracks: {albumLength}
         </Typography>
       </CardContent>
       <CardActions>
